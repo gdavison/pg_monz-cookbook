5e6854500e0fb289e6fca409de21be222a2c7a89
Graham Davison <gdavison@paypoint.com>
2015-03-26 15:23:08 -0700

    ITOPS-5662 bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ license          'All rights reserved'
 description      'Installs/Configures pg_monz'
 long_description 'Installs/Configures pg_monz'
 
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'postgresql',   '~> 3.4.14'
 depends 'zabbix-agent', '~> 0.10.0'
