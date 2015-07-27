39ab33684ebcac62fa85b0351ccacee1ee7d0298
Graham Davison <gdavison@paybyphone.com>
2015-07-27 16:36:35 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'Apache v2.0'
 description      'Installs/Configures pg_monz'
 long_description 'Installs/Configures pg_monz'
 
-version          '0.1.2'
+version          '0.1.3'
 
 depends 'postgresql',   '~> 3.4'
 depends 'zabbix-agent', '~> 0.10'
